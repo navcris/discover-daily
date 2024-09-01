262142e178f00bde6397f2faf8b22f42a31e6634
navcris <177436722+navcris@users.noreply.github.com>
2024-08-31 23:12:39 -0700

    Add files via upload

---
+++ src/components/AboutBody.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./AboutBody.css";
+import { Link } from "react-router-dom";
+
+const AboutBody = () => {
+  return (
+    <div className="about-article-container">
+      <div className="about-article-wrapper">
+        <div className="about-article-body">
+          <p>
+            <b>Discovery Daily</b> is a web app designed to keep young students
+            informed on STEM news in a classroom-safe environment. Inspired by a
+            previous teacher's beloved NewsRoom activity, Discovery Daily
+            scrapes and filters news articles from various sources focused on
+            technology, environment, and space. This allows students to keep up
+            on current events without the worry of encountering politically
+            sensitive content.
+          </p>
+          <h2>About the Creator</h2>
+          <img src="public\about.JPG" className="about-picture"></img>
+          <p>
+            Hello! My name is Navarro Cristani and I am a 19 year old aspiring
+            Software Engineer passionate about teaching, learning, and making
+            technology accessible to all. My interest in tech started at a young
+            age with an intense curiosity about the ever-evolving world around
+            me. This deep interest has led me to learn and collaborate with
+            others in tech and education, sharing my current knowledge as much
+            as possible.
+          </p>
+          <div className="contact-wrapper">
+            <h4 className="contacts"> Contact Me!</h4>
+            <h4 className="contacts">
+              <Link
+                className="linkedin-link"
+                to="https://www.linkedin.com/in/navarro-cristani/"
+              >
+                LinkedIn
+              </Link>
+              <Link
+                className="email-link"
+                to="javascript:void(0)"
+                onClick={() =>
+                  (window.location.href = "mailto:yourmail@domain.com")
+                }
+              >
+                Email
+              </Link>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutBody;
---
+++ src/components/AboutHero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./AboutHero.css";
+
+const AboutHero = () => {
+  return (
+    <div>
+      <div className="about-hero-container">
+        <img className="about-main-img" src={"public/classroom.jpg"}></img>
+        <div className="about-title-container">
+          <h1 className="about-main-title">About</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutHero;
