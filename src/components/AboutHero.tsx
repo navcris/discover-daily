b91ca0641cef9c3a8008b313f075391d3c6b27c2
navcris <177436722+navcris@users.noreply.github.com>
2024-09-02 19:50:56 -0700

    Update AboutHero.tsx

--- src/components/AboutHero.tsx
+++ src/components/AboutHero.tsx
@@ -5,7 +5,7 @@ const AboutHero = () => {
   return (
     <div>
       <div className="about-hero-container">
-        <img className="about-main-img" src={"./public/classroom.jpg"}></img>
+        <img className="about-main-img" src={"public/classroom.jpg"}></img>
         <div className="about-title-container">
           <h1 className="about-main-title">About</h1>
         </div>
