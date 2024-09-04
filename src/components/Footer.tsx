c6cc0b7fd053663a8f7795d8cfc8695aff054f38
navcris <177436722+navcris@users.noreply.github.com>
2024-09-03 22:41:32 -0700

    ok now it should be good

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,42 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 const Footer = () => {
-  const [footer, setFooter] = useState(false);
   const handleClick = () => {
     window.scroll(0, 0);
   };
 
-  const checkScreen = () => {
-    if (window.innerWidth <= 600) {
-      setFooter(true);
-    } else {
-      setFooter(false);
-    }
-  };
-
-  useEffect(() => {
-    checkScreen();
-  }, []);
-
-  window.addEventListener("resize", checkScreen);
-
-  return footer == false ? (
+  return (
     <div className="footer-container">
       <div className="footer-brand">
-        <div className="text" onClick={handleClick}>
-          Take me to the Top
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="footer-container">
-      <div className="footer-brand">
-        <div className="About">
-          <Link to="/about" className="custom-link">
-            About
-          </Link>
-        </div>
-
         <div className="text" onClick={handleClick}>
           Take me to the Top
         </div>
